8d29b021a7633fd648a2cc00571df08898c21bb6
shivsagar mishra <shivsagarm50@gmail.com>
2025-02-27 11:52:19 +0530

    added fibbonacci prohram too

--- gpt.py
+++ gpt.py
@@ -10,3 +10,19 @@ def factorial(n):
 #driver fucntion
 n = 5
 print("Factorial of", n, "is", factorial(n))
+
+
+#write code for  fibonacci series
+def fibonacci(n):
+    if n <= 0:
+        return "Input should be a positive integer."
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+    #driver function
+n = 10
+print("Fibonacci series of", n, "is", fibonacci(n))
+
