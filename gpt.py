fea3ac53bbddf7c86b2a405279c9e29ee3551020
shivsagar mishra <shivsagarm50@gmail.com>
2025-02-27 11:48:52 +0530

    added factorial program

--- gpt.py
+++ gpt.py
@@ -1,1 +1,12 @@
-print("hi, this is gpt.py")
+#print("hi, this is gpt.py")
+
+#write code for  factorial
+
+def factorial(n):   
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+#driver fucntion
+n = 5
+print("Factorial of", n, "is", factorial(n))
